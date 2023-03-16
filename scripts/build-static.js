99f95bcb2f8181ba582b1fc31efefb0fa17ba8bc
Le Vivilet <levivlet@pm.me>
2023-03-16 21:51:28 +0100

    fix: static build

--- scripts/build-static.js
+++ scripts/build-static.js
@@ -1,3 +1,10 @@
 import { exportStatic } from '@lvce-editor/shared-process'
+import path, { dirname } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
-await exportStatic()
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const root = path.join(__dirname, '..')
+
+await exportStatic({
+  root,
+})
